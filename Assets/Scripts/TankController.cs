c463201304b1d96616b6b97033b8b292b5d273f1
explosiveseccc <rdericieux25@aoiths.org>
2023-09-20 12:25:20 -0400

    1.4 done

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
-    public float speed = 20; 
+    public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;   
     
     // Start is called before the first frame update
     void Start()
@@ -15,9 +18,14 @@ public class TankController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); /*
+        horizontalInput = Input.GetAxis("Horizontal"); // this will get the horizontal inputs from unity. You can customize what the inputs are by doing edit > Project Settings. This will allow you to customize the inputs of your axis but its best not to do that unless nessarcy. 
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); /*
         remeber that time.deltatime makes the game run by the seconds in the game instead of the amount of frames you have while running the game
         Vector 3 forward is just (0,0,1) but itsa just "easier" to read for later
         */
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); // this will affect how fast your character will be able to turn 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
