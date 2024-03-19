8ea4a49c86d5efada3f89c39cf5f03709130ab78
explosiveseccc <rdericieux25@aoiths.org>
2024-03-19 12:44:15 -0400

    ea

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     public GameObject Tank;
+    public bool FirstPersonView; 
     Vector3 Offset = new Vector3(0, 5, -15); // this will be used to offset the camera from the player so that the player can see the character they are playing as
+    Vector3 FirstPov = new Vector3 (0, 5, 0);
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            FirstPersonView = !FirstPersonView;
+        }
     }
 
     // Update is called once per frame
     void LateUpdate() //late update is a method that runs during ending of the fame the game is runned. In other words this script is runned as the last priority for the game for every frame the game is running. 
     {
-        transform.position = Tank.transform.position + Offset; // this will constanlty put the camera in the same position with the players character with a offset so that the player can see their character
+        if (FirstPersonView == false)
+        {
+         transform.position = Tank.transform.position + Offset; // this will constanlty put the camera in the same position with the players character with a offset so that the player can see their character
+        }
+        else
+        {
+            transform.position = Tank.transform.position + FirstPov;
+        }
     }
 }
